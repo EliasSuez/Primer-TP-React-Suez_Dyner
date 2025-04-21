6bcf0d7b1af135b61728db795e058c22872b8519
EliasSuez <48240167@est.ort.edu.ar>
2025-04-21 14:14:50 -0300

    Cambios en el proyecto

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Cita from './Cita'
 
+import './Listado.css'
 function Listado(){
     
 
 
     return(
-        <div>
+        <div class="one-half column">
+            <h2>Administra tus citas</h2>
             <Cita mascota='Nina' duenio='Martin' fecha='2021-08-05' hora='08:20' sintomas='Le duele la pierna'></Cita>
             <Cita mascota='Sifon' duenio='Flecha' fecha='2023-08-05' hora='09:24' sintomas='Duerme mucho'></Cita>
             <Cita mascota='Floki' duenio='Ari' fecha='2023-08-05' hora='16:15' sintomas='No está comiendo'></Cita>
