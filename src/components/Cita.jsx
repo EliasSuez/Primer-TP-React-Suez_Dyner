51efc694bcd03d4e3c91d87aa85c04e1d443b680
EliasSuez <48240167@est.ort.edu.ar>
2025-04-21 14:23:11 -0300

    Citas creadas

--- src/components/Cita.jsx
+++ src/components/Cita.jsx
@@ -4,12 +4,12 @@ import './Cita.css'
 function Cita(props){
 
     return(
-        <div class="cita">
+        <div className="cita">
             <p>Mascota: <span>{props.mascota}</span></p>
             <p>Dueño: <span>{props.duenio}</span></p>
             <p>Fecha: <span>{props.fecha}</span></p>
             <p>Hora: <span>{props.hora}</span></p>
-            <p>Sintomas: <span>{props.sintomas}</span></p><button class="button elimnar u-full-width">Eliminar ×</button>
+            <p>Sintomas: <span>{props.sintomas}</span></p><button className="button elimnar u-full-width">Eliminar ×</button>
         </div>
     )
 }
--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -8,7 +8,7 @@ function Listado(){
 
 
     return(
-        <div class="one-half column">
+        <div className="one-half column">
             <h2>Administra tus citas</h2>
             <Cita mascota='Nina' duenio='Martin' fecha='2021-08-05' hora='08:20' sintomas='Le duele la pierna'></Cita>
             <Cita mascota='Sifon' duenio='Flecha' fecha='2023-08-05' hora='09:24' sintomas='Duerme mucho'></Cita>
