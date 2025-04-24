2810b51ba3674d6ada9fe1c03edb33cefdd69b17
EliasSuez <48240167@est.ort.edu.ar>
2025-04-24 11:22:30 -0300

    Cambios en el css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 import Listado from './components/Listado'
@@ -9,8 +7,9 @@ function App() {
 
   return (
     <>
-      <Listado></Listado>
-      <Formulario></Formulario>
+      <Formulario/>
+      <Listado/>
+      
     </>
   )
 }
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './Formulario.css'
+
 function Formulario() {
 
 
